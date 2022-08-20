d28f16472b38f76a8f99580533cc1614080c27c5
panguojun <panguojun@users.noreply.github.com>
2022-08-20 11:29:48 +0000

    Update coord.hpp

--- coord.hpp
+++ coord.hpp
@@ -88,9 +88,9 @@ struct coord_t
 	}
 	vec3 cross(const coord_t& c)
 	{
-		return 	(ux.cross(c.ux) * c.scl.x + ux.dot(c.uy) * c.scl.y + ux.cross(c.uz) * c.scl.z) +
-			(uy.cross(c.ux) * c.scl.x + uy.dot(c.uy) * c.scl.y + uy.cross(c.uz) * c.scl.z) +
-			(uz.cross(c.ux) * c.scl.x + uz.dot(c.uy) * c.scl.y + uz.cross(c.uz) * c.scl.z);
+		return 	(ux.cross(c.ux) * c.scl.x + ux.cross(c.uy) * c.scl.y + ux.cross(c.uz) * c.scl.z) +
+			(uy.cross(c.ux) * c.scl.x + uy.cross(c.uy) * c.scl.y + uy.cross(c.uz) * c.scl.z) +
+			(uz.cross(c.ux) * c.scl.x + uz.cross(c.uy) * c.scl.y + uz.cross(c.uz) * c.scl.z);
 	}
 	void dump()
 	{
