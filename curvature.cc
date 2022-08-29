4631e629a436152331683cc8eca01365c7832bc6
panguojun <panguojun@users.noreply.github.com>
2022-08-29 13:41:04 +0000

    Update curvature.cc

--- curvature.cc
+++ curvature.cc
@@ -1,15 +1,18 @@
-void coord(vec3& cv, vec3& v)
+vec3 mul_coord(const vec3& v)
 {
+    vec3 cv; 
     cv.x = x;
     cv.y = 2.0 * y;
+    return cv;
 }
-
+vec3 div_coord(const vec3& cv)
+{
+    vec3 v; 
+    v.x = cx;
+    v.y = cy / 2.0;
+    return v;
+}
 void curvature()
 {
-	coord c;
-	auto A = [](crvec p, real t)->vec3 {
-		vec3 dp = p;
-		return dp * sin(t); 
-	};
-	
+		
 }
