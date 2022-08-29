edb8302d639e48012e02acc32afee7e5f1723889
panguojun <panguojun@users.noreply.github.com>
2022-08-29 14:16:49 +0000

    Update curvature.cc

--- curvature.cc
+++ curvature.cc
@@ -1,21 +1,13 @@
-vec3 mul_coord(const vec3& v)
-{
-    vec3 cv; 
-    cv.x = x;
-    cv.y = 2.0 * y;
-    return cv;
-}
-vec3 div_coord(const vec3& cv)
-{
-    vec3 v; 
-    v.x = cx;
-    v.y = cy / 2.0;
-    return v;
-}
 void curvature()
 {
-	coord3 c;
 	vec3 v = vec3(1,0,0);
-	vec3 cv = v * c;
-	
+	coord3 c11(v);
+	coord3 c21(c11.o + vec3::UX * deta_d);
+	coord3 c12(c11.o + vec3::UY * deta_d);
+	coord3 c22_21(c21.o + vec3::UY * deta_d);
+	coord3 c22_12(c12.o + vec3::UX * deta_d);
+	vec3 v11 = v * c11;
+	vec3 v12 = v * c12;
+	vec3 v21 = v * c21;
+	vec3 v22 = v * c22;
 }
