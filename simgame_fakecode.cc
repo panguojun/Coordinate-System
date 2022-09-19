6773c66d7e33a384e3cc188bc1552d3cd6c1761b
panguojun <panguojun@users.noreply.github.com>
2022-09-19 15:19:14 +0000

    Update simgame_fakecode.cc

--- simgame_fakecode.cc
+++ simgame_fakecode.cc
@@ -12,13 +12,14 @@
             return: coord(vec2(cos(x),sin(y)),vec2(sin(x),cos(y)));
         }
         mainimage{
-            coord1:polar_coord(x,y);
-            dx1:x-0.25;dy:y-0.5;
-            r1:sqrt(dx1*dx1+dy1*dy1);
-            dx2:x-0.75;dy:y-0.5;
-            r2:sqrt(dx2*dx2+dy2*dy2);
-            f:sin(r1)+sin(r2);
-            color: rgb(f,f,0);
+            coord1:=polar_coord(x,y);   // coord
+            dx:=x-0.25;dy:=y-0.5;   
+            r1:=sqrt(dx*dx+dy*dy);
+            dx=x-0.75;dy:y-0.5;
+            r2:=sqrt(dx*dx+dy*dy);
+            
+            f:=sin(r1)+sin(r2);         // field
+            color=rgb(f,f,0);           // display
         }
     }
 }
