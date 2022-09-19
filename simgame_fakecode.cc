c45da9eb878135efeed00808e81ff97872815799
panguojun <panguojun@users.noreply.github.com>
2022-09-19 15:16:19 +0000

    Update simgame_fakecode.cc

--- simgame_fakecode.cc
+++ simgame_fakecode.cc
@@ -1,12 +1,13 @@
-{   #gui
+{#sim game gui and shader fake code
+    params{param1:1;param2:2;param3:3}
+    #gui
     gui{
-        params{param1:1;param2:2;param3:3}
         btn1{type:button;p:100,10;onclick:param1++}
         btn2{type:button;p:100,30;onclick:param2++}
     }
     #shader
     shader2d{
-        params{mouse:1,1}
+        shaderparams{mouse:1,1;param1:0;param2:0}
         polar_coord{params:float x, float y;
             return: coord(vec2(cos(x),sin(y)),vec2(sin(x),cos(y)));
         }
