0adcdd80cdc7340dd2365cb5f0b78f2b3a1258ed
panguojun <panguojun@users.noreply.github.com>
2022-09-19 14:42:39 +0000

    Update fakecode.cc

--- fakecode.cc
+++ fakecode.cc
@@ -1,12 +1,15 @@
 
-{#gui
-  gui{
-    params:{param1:1;param2:2;param3:3}
-    btn1{type:button;p:100,10;onclick:onbtn1}
-    btn2{type:button;p:100,30;onclick:btn2}
-  }
-  #shader
-  shader{
-    
-  }
+{   #gui
+    gui{
+        params{param1:1;param2:2;param3:3}
+        btn1{type:button;p:100,10;onclick:onbtn1}
+        btn2{type:button;p:100,30;onclick:btn2}
+    }
+    #shader
+    shader{
+        params{mouse:1,1}
+        mainimage{
+            color = rgb(1,1,0);
+        }
+    }
 }
