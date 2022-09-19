0bbfd05ca21b157d3f0c0868dabaf8a0835b42ee
panguojun <panguojun@users.noreply.github.com>
2022-09-19 14:34:15 +0000

    Create fakecode.cc

---
+++ fakecode.cc
@@ -0,0 +1,12 @@
+
+{#gui
+  gui{
+    params:{param1:1;param2:2;param3:3}
+    btn1{type:button;p:100,10;onclick:onbtn1}
+    btn2{type:button;p:100,30;onclick:btn2}
+  }
+  #shader
+  shader{
+    
+  }
+}
