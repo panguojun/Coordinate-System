14cfdedec1705c3485edc87f5e001ecc8935a088
panguojun <panguojun@users.noreply.github.com>
2022-11-01 11:12:41 +0800

    Add files via upload

---
+++ quaternion.hpp
@@ -0,0 +1,346 @@
+/****************************************************************************
+							四元数
+					(本文件基于OGRE引擎的数学库)
+	四元数是在复数基础上的扩展,单位四元数用于旋转操作，向量是源自于四元数，
+	不过二者有差别。目前四元数跟向量之间的关系以及应用存在着争议。
+
+	*  *  *  *  *  *  *  *  *  详解  *  *  *  *  *  *  *  *  *  *  *  *  * 
+	类似于复数，四元数也拥有指数形式：e^q，结果也是一个四元数： q = e^q,
+	在底层物理里应了规范变换，跟坐标系变换有一些不同，规范变换更加
+	侧重相位拥有时间属性，坐标系变换偏向于空间的变换以及曲率等特征提取。
+
+	四元数存在归一化(normalise)，共轭(conj)，求逆(invert)，乘除法等操作，
+	还规定了单位一（ONE).
+
+****************************************************************************/
+struct  quaternion
+{
+	real w, x, y, z;
+	static const quaternion ONE;
+	//-----------------------------------------------------------------------
+	quaternion(
+		real fW = 1.0,
+		real fX = 0.0, real fY = 0.0, real fZ = 0.0)
+	{
+		w = fW;
+		x = fX;
+		y = fY;
+		z = fZ;
+	}
+	quaternion(const quaternion& rkQ)
+	{
+		w = rkQ.w;
+		x = rkQ.x;
+		y = rkQ.y;
+		z = rkQ.z;
+	}
+	quaternion(const real& rfAngle, const vector3& rkAxis)
+	{
+		this->fromangleaxis(rfAngle, rkAxis);
+	}
+	//-----------------------------------------------------------------------
+	quaternion operator+ (const quaternion& rkQ) const
+	{
+		return quaternion(w + rkQ.w, x + rkQ.x, y + rkQ.y, z + rkQ.z);
+	}
+	//-----------------------------------------------------------------------
+	quaternion operator- (const quaternion& rkQ) const
+	{
+		return quaternion(w - rkQ.w, x - rkQ.x, y - rkQ.y, z - rkQ.z);
+	}
+	quaternion operator - () const
+	{
+		quaternion q;
+		q.x = -x;
+		q.y = -y;
+		q.z = -z;
+		q.w = -w;
+		return q;
+	}
+	//-----------------------------------------------------------------------
+	vector3 operator* (const vector3& v) const
+	{
+		// nVidia SDK implementation
+		vector3 uv, uuv;
+		vector3 qvec(x, y, z);
+		uv = qvec.cross(v);
+		uuv = qvec.cross(uv);
+		uv = uv * (2.0f * w);
+		uuv = uuv * 2.0f;
+
+		return v + uv + uuv;
+	}
+	vector3 friend operator* (const vector3& v, const quaternion& q)
+	{
+		return q * v;
+	}
+	quaternion operator* (const quaternion& rkQ) const
+	{
+		// NOTE:  Multiplication is not generally commutative, so in most
+		// cases p*q != q*p.
+
+		return quaternion
+		(
+			w * rkQ.w - x * rkQ.x - y * rkQ.y - z * rkQ.z,
+			w * rkQ.x + x * rkQ.w + y * rkQ.z - z * rkQ.y,
+			w * rkQ.y + y * rkQ.w + z * rkQ.x - x * rkQ.z,
+			w * rkQ.z + z * rkQ.w + x * rkQ.y - y * rkQ.x
+		);
+	}
+	quaternion operator* (real fScalar) const
+	{
+		return quaternion(fScalar * w, fScalar * x, fScalar * y, fScalar * z);
+	}
+	//-----------------------------------------------------------------------
+	quaternion friend operator* (real fScalar, const quaternion& rkQ)
+	{
+		return quaternion(fScalar * rkQ.w, fScalar * rkQ.x, fScalar * rkQ.y,
+			fScalar * rkQ.z);
+	}
+	//-----------------------------------------------------------------------
+	quaternion operator / (real fScalar) const
+	{
+		return quaternion(w / fScalar, x / fScalar, y / fScalar, z / fScalar);
+	}
+	quaternion operator / (const quaternion& q) const
+	{
+		return (*this) * q.invertcopy();
+	}
+	vector3 friend operator/ (const vector3& v, const quaternion& q)
+	{
+		return q.invertcopy() * v;
+	}
+	//-----------------------------------------------------------------------
+	bool operator == (const quaternion& rkQ) const
+	{
+		return w == rkQ.w && x == rkQ.x && y == rkQ.y && z == rkQ.z;
+	}
+	//-----------------------------------------------------------------------
+	real dot(const quaternion& rkQ) const
+	{
+		return w * rkQ.w + x * rkQ.x + y * rkQ.y + z * rkQ.z;
+	}
+	//-----------------------------------------------------------------------
+	real length() const
+	{
+		return sqrt(w * w + x * x + y * y + z * z);
+	}
+	//-----------------------------------------------------------------------
+	void normalise(void)
+	{
+		real len = length();
+		if (len != 0)
+		{
+			real factor = 1.0f / (len);
+			*this = *this * factor;
+		}
+	}
+	//-----------------------------------------------------------------------
+	void conj() // 共轭
+	{
+		this->w = w;
+		this->x = -x; this->y = -y; this->z = -z;
+	}
+	quaternion conjcopy()
+	{
+		quaternion c;
+		c.w = w;
+		c.x = -x; c.y = -y; c.z = -z;
+		return c;
+	}
+	//-----------------------------------------------------------------------
+	void invert()
+	{
+		real r = length();
+		if (r != 0)
+		{
+			quaternion q = (*this) / r;
+			q.conj();
+			(*this) = q / r;
+		}
+	}
+	quaternion invertcopy() const
+	{
+		real r = length();
+		if (r == 0)
+			return ONE;
+		quaternion q = (*this) / r;
+		q.conj();
+		return q / r;
+	}
+	//-----------------------------------------------------------------------
+	// 指数四元数就是规范变换！
+	quaternion exp()
+	{
+		real r = length();
+		vec3 v(x, y, z);
+		real th = v.len();
+		vec3 n = v.normcopy();
+		vec3 qv = n * (r * sin(th));
+		return quaternion(r * cos(th), qv.x, qv.y, qv.z);
+	}
+	//-----------------------------------------------------------------------
+	void fromangleaxis(real rfAngle,
+		const vector3& rkAxis)
+	{
+		// assert:  axis[] is unit length
+		//
+		// The quaternion representing the rotation is
+		//   q = cos(A/2)+sin(A/2)*(x*i+y*j+z*k)
+
+		real fHalfAngle(0.5 * rfAngle);
+		real fSin = sin(fHalfAngle);
+		w = cos(fHalfAngle);
+		x = fSin * rkAxis.x;
+		y = fSin * rkAxis.y;
+		z = fSin * rkAxis.z;
+	}
+	//-----------------------------------------------------------------------
+	void fromeuler(real roll, real pitch, real yaw)
+	{
+		real t0 = cos(yaw * 0.5);
+		real t1 = sin(yaw * 0.5);
+		real t2 = cos(roll * 0.5);
+		real t3 = sin(roll * 0.5);
+		real t4 = cos(pitch * 0.5);
+		real t5 = sin(pitch * 0.5);
+
+		w = t2 * t4 * t0 + t3 * t5 * t1;
+		x = t3 * t4 * t0 - t2 * t5 * t1;
+		y = t2 * t5 * t0 + t3 * t4 * t1;
+		z = t2 * t4 * t1 - t3 * t5 * t0;
+	}
+	//-----------------------------------------------------------------------
+	vec3 xyz() const
+	{
+		return vec3(x, y, z);
+	}
+	vec3 tovec() const
+	{
+		return vec3(x, y, z);
+	}
+	//-----------------------------------------------------------------------
+	vec3 toeuler()
+	{
+		vec3 v;
+
+		real epsilon = 0.00001;
+		real halfpi = 0.5 * PI;
+
+		real temp = 2 * (y * z - x * w);
+		if (temp >= 1 - epsilon)
+		{
+			v.x = halfpi;
+			v.y = -atan2(y, w);
+			v.z = -atan2(z, w);
+		}
+		else if (-temp >= 1 - epsilon)
+		{
+			v.x = -halfpi;
+			v.y = -atan2(y, w);
+			v.z = -atan2(z, w);
+		}
+		else
+		{
+			v.x = asin(temp);
+			v.y = -atan2(x * z + y * w, 0.5 - x * x - y * y);
+			v.z = -atan2(x * y + z * w, 0.5 - x * x - z * z);
+		}
+
+		return v;
+	}
+	//-----------------------------------------------------------------------
+	quaternion slerp(const quaternion& qa, const quaternion& qb, double t) {
+		// quaternion to return
+		quaternion qm;
+		// Calculate angle between them.
+		double cosHalfTheta = qa.w * qb.w + qa.x * qb.x + qa.y * qb.y + qa.z * qb.z;
+		// if qa=qb or qa=-qb then theta = 0 and we can return qa
+		if (abs(cosHalfTheta) >= 1.0) {
+			qm.w = qa.w; qm.x = qa.x; qm.y = qa.y; qm.z = qa.z;
+			return qm;
+		}
+		// Calculate temporary values.
+		double halfTheta = acos(cosHalfTheta);
+		double sinHalfTheta = sqrt(1.0 - cosHalfTheta * cosHalfTheta);
+		// if theta = 180 degrees then result is not fully defined
+		// we could rotate around any axis normal to qa or qb
+		if (fabs(sinHalfTheta) < 0.001) { // fabs is floating point absolute
+			qm.w = (qa.w * 0.5 + qb.w * 0.5);
+			qm.x = (qa.x * 0.5 + qb.x * 0.5);
+			qm.y = (qa.y * 0.5 + qb.y * 0.5);
+			qm.z = (qa.z * 0.5 + qb.z * 0.5);
+			return qm;
+		}
+		double ratioA = sin((1 - t) * halfTheta) / sinHalfTheta;
+		double ratioB = sin(t * halfTheta) / sinHalfTheta;
+		//calculate Quaternion.
+		qm.w = (qa.w * ratioA + qb.w * ratioB);
+		qm.x = (qa.x * ratioA + qb.x * ratioB);
+		qm.y = (qa.y * ratioA + qb.y * ratioB);
+		qm.z = (qa.z * ratioA + qb.z * ratioB);
+		return qm;
+	}
+	//-----------------------------------------------------------------------
+	quaternion slerp(real fT, const quaternion& rkP,
+		const quaternion& rkQ, bool shortestPath)
+	{
+		const real msEpsilon = 1e-03;
+		real fCos = rkP.dot(rkQ);
+		quaternion rkT;
+
+		// Do we need to invert rotation?
+		if (fCos < 0.0f && shortestPath)
+		{
+			fCos = -fCos;
+			rkT = -rkQ;
+		}
+		else
+		{
+			rkT = rkQ;
+		}
+
+		if (fabs(fCos) < 1 - msEpsilon)
+		{
+			// Standard case (slerp)
+			real fSin = sqrt(1 - (fCos * fCos));
+			real fAngle = atan2(fSin, fCos);
+			real fInvSin = 1.0f / fSin;
+			real fCoeff0 = sin((1.0f - fT) * fAngle) * fInvSin;
+			real fCoeff1 = sin(fT * fAngle) * fInvSin;
+			return fCoeff0 * rkP + fCoeff1 * rkT;
+		}
+		else
+		{
+			// There are two situations:
+			// 1. "rkP" and "rkQ" are very close (fCos ~= +1), so we can do a linear
+			//    interpolation safely.
+			// 2. "rkP" and "rkQ" are almost inverse of each other (fCos ~= -1), there
+			//    are an infinite number of possibilities interpolation. but we haven't
+			//    have method to fix this case, so just use linear interpolation here.
+			quaternion t = (1.0f - fT) * rkP + fT * rkT;
+			// taking the complement requires renormalisation
+			t.normalise();
+			return t;
+		}
+	}
+	//-----------------------------------------------------------------------
+	quaternion nlerp(real fT, const quaternion& rkP,
+		const quaternion& rkQ, bool shortestPath)
+	{
+		quaternion result;
+		real fCos = rkP.dot(rkQ);
+		if (fCos < 0.0f && shortestPath)
+		{
+			result = rkP + fT * ((-rkQ) - rkP);
+		}
+		else
+		{
+			result = rkP + fT * (rkQ - rkP);
+		}
+		result.normalise();
+		return result;
+	}
+};
+// **********************************************************************
+const quaternion quaternion::ONE = quaternion(1, 0, 0, 0);
---
+++ vector.hpp
@@ -0,0 +1,689 @@
+// **********************************************************************
+// 2D
+// **********************************************************************
+struct vector2 {
+	union {
+		real val[2];
+		struct {
+			real x;
+			real y;
+		};
+	};
+
+	static const vector2 ZERO;
+	static const vector2 ONE;
+	static const vector2 UX;
+	static const vector2 UY;
+	static const vector2 CENTER;
+
+	real& operator [](int ind) {
+		return val[ind];
+	}
+
+	vector2() {
+		x = 0;
+		y = 0;
+	}
+	vector2(real v)
+	{
+		x = v;
+		y = v;
+	}
+	vector2(real _x, real _y) {
+		x = _x;
+		y = _y;
+	}
+	vector2 operator + (const vector2& _p) const
+	{
+		vector2 fp;
+		fp.x = x + _p.x;
+		fp.y = y + _p.y;
+
+		return fp;
+	}
+	void operator += (const vector2& _p)
+	{
+		x += _p.x;
+		y += _p.y;
+	}
+	vector2 operator - (const vector2& _p) const
+	{
+		vector2 fp;
+		fp.x = x - _p.x;
+		fp.y = y - _p.y;
+		return fp;
+	}
+	void operator -= (const vector2& _p)
+	{
+		x = x - _p.x;
+		y = y - _p.y;
+	}
+	vector2 operator - () const
+	{
+		vector2 fp;
+		fp.x = -x;
+		fp.y = -y;
+		return fp;
+	}
+	vector2 operator * (real s) const
+	{
+		vector2 fp;
+		fp.x = s * x;
+		fp.y = s * y;
+		return fp;
+	}
+	void operator *= (real s)
+	{
+		x = s * x;
+		y = s * y;
+	}
+	friend vector2 operator * (real s, const vector2& v)
+	{
+		vector2 fp;
+		fp.x = v.x * s;
+		fp.y = v.y * s;
+		return fp;
+	}
+	vector2 operator * (const vector2& b) const
+	{
+		return vector2(x * b.x - y * b.y, x * b.y + y * b.x);
+	}
+	vector2 operator / (real s) const
+	{
+		vector2 fp;
+		fp.x = x / s;
+		fp.y = y / s;
+		return fp;
+	}
+	void operator /= (real s)
+	{
+		x = x / s;
+		y = y / s;
+	}
+	bool operator == (const vector2& rv) const
+	{
+		return (fabs(x - rv.x) <= 1e-5 && fabs(y - rv.y) <= 1e-5);
+	}
+	bool operator != (const vector2& rv) const
+	{
+		return (fabs(x - rv.x) > 1e-5 || fabs(y - rv.y) > 1e-5);
+	}
+	real len() const
+	{
+		return sqrt(x * x + y * y);
+	}
+	real sqrlen() const
+	{
+		return (x * x + y * y);
+	}
+	real angle() const
+	{
+		return atan2(y, x);
+	}
+	void norm()
+	{
+		real r = len();
+		if (r > 0)
+		{
+			x /= r;
+			y /= r;
+		}
+	}
+	vector2 normcopy() const
+	{
+		real r = len();
+		if (r > 0)
+		{
+			return vector2(x / r, y / r);
+		}
+		return vector2::ZERO;
+	}
+	void rot(real angle)
+	{
+		(*this) = (*this) * vector2::fromanglelength(angle, 1);
+	}
+	vector2 rotcopy(real angle) const
+	{
+		return (*this) * vector2::fromanglelength(angle, 1);
+	}
+	void rot(real angle, const vector2& o)
+	{
+		vector2 v = (*this) - o;
+		v = v * vector2::fromanglelength(angle, 1);
+		(*this) = v + o;
+	}
+	vector2 rotcopy(real angle, const vector2& o) const
+	{
+		vector2 v = (*this) - o;
+		v = v * vector2::fromanglelength(angle, 1);
+		return v + o;
+	}
+	vector2 rotcpy(real angle, const vector2& o) const
+	{
+		vector2 v = (*this) - o;
+		v = v * vector2::fromanglelength(angle, 1);
+		return v + o;
+	}
+	real dot(const vector2& v) const
+	{
+		return x * v.x + y * v.y;
+	}
+	real cross(const vector2& v) const
+	{
+		return x * v.y - y * v.x;
+	}
+	static vector2 fromanglelength(real _angle, real _r);
+};
+
+const vector2 vector2::ZERO = vector2(0, 0);
+const vector2 vector2::ONE = vector2(1, 1);
+const vector2 vector2::UX = vector2(1, 0);
+const vector2 vector2::UY = vector2(0, 1);
+const vector2 vector2::CENTER = vector2(0.5, 0.5);
+
+vector2 vector2::fromanglelength(real _angle, real _r)
+{
+	return vector2(_r * cos(_angle), _r * sin(_angle));
+}
+
+// **********************************************************************
+// 3D
+// **********************************************************************
+struct vector3
+{
+	static const vector3 ZERO;
+	static const vector3 ONE;
+	static const vector3 UX;
+	static const vector3 UY;
+	static const vector3 UZ;
+	static const vector3 CENTER;
+	static real sEPSINON;
+	union {
+		real val[3];
+		struct
+		{
+			real x;
+			real y;
+			real z;
+		};
+		struct
+		{
+			real r;
+			real g;
+			real b;
+		};
+	};
+	vector3()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+	explicit vector3(real v)
+	{
+		x = v;
+		y = v;
+		z = v;
+	}
+	vector3(real _x, real _y, real _z = 0.0f)
+	{
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+	vector3(const vector3& v)
+	{
+		x = v.x;
+		y = v.y;
+		z = v.z;
+	}
+	real& operator [](int ind) {
+		return val[ind];
+	}
+	vector2 xy() const
+	{
+		vector2 fp;
+		fp.x = x;
+		fp.y = y;
+		return fp;
+	}
+	void xy(const vec2& _xy)
+	{
+		x = _xy.x;
+		y = _xy.y;
+	}
+	vector2 xz() const
+	{
+		vector2 fp;
+		fp.x = x;
+		fp.y = z;
+		return fp;
+	}
+	void xz(const vec2& _xz)
+	{
+		x = _xz.x;
+		z = _xz.y;
+	}
+	vector2 yz() const
+	{
+		vector2 fp;
+		fp.x = y;
+		fp.y = z;
+		return fp;
+	}
+	vector3 zxy() const
+	{
+		vector3 fp;
+		fp.x = z;
+		fp.y = x;
+		fp.z = y;
+		return fp;
+	}
+	vector3 operator + (const vector3& _p) const
+	{
+		vector3 fp;
+		fp.x = x + _p.x;
+		fp.y = y + _p.y;
+		fp.z = z + _p.z;
+		return fp;
+	}
+	void operator += (const vector3& _p)
+	{
+		x += _p.x;
+		y += _p.y;
+		z += _p.z;
+	}
+	vector3 operator - (const vector3& _p) const
+	{
+		vector3 fp;
+		fp.x = x - _p.x;
+		fp.y = y - _p.y;
+		fp.z = z - _p.z;
+		return fp;
+	}
+	void operator -= (const vector3& _p)
+	{
+		x -= _p.x;
+		y -= _p.y;
+		z -= _p.z;
+	}
+	vector3 operator - () const
+	{
+		vector3 fp;
+		fp.x = -x;
+		fp.y = -y;
+		fp.z = -z;
+		return fp;
+	}
+	vector3 operator * (real s) const
+	{
+		vector3 fp;
+		fp.x = s * x;
+		fp.y = s * y;
+		fp.z = s * z;
+		return fp;
+	}
+	vector3 operator * (const vector3& v) const
+	{
+		vector3 fp;
+		fp.x = v.x * x;
+		fp.y = v.y * y;
+		fp.z = v.z * z;
+		return fp;
+	}
+	friend vector3 operator * (real s, const vector3& v)
+	{
+		vector3 fp;
+		fp.x = v.x * s;
+		fp.y = v.y * s;
+		fp.z = v.z * s;
+		return fp;
+	}
+	void operator *= (real s)
+	{
+		x = s * x;
+		y = s * y;
+		z = s * z;
+	}
+	vector3 operator / (real s) const
+	{
+		vector3 fp;
+		fp.x = x / s;
+		fp.y = y / s;
+		fp.z = z / s;
+		return fp;
+	}
+	void operator /= (real s)
+	{
+		x = x / s;
+		y = y / s;
+		z = z / s;
+	}
+	bool operator == (const vector3& rv) const
+	{
+		return (fabs(x - rv.x) <= sEPSINON && fabs(y - rv.y) <= sEPSINON && fabs(z - rv.z) <= sEPSINON);
+	}
+	bool operator != (const vector3& rv) const
+	{
+		return (fabs(x - rv.x) > sEPSINON || fabs(y - rv.y) > sEPSINON || fabs(z - rv.z) > sEPSINON);
+	}
+	bool operator < (const vector3& rv) const
+	{
+		if (x < rv.x && y < rv.y && z < rv.z)
+			return true;
+		return false;
+	}
+	bool operator > (const vector3& rv) const
+	{
+		if (x > rv.x && y > rv.y && z > rv.z)
+			return true;
+		return false;
+	}
+	real len() const
+	{
+		return sqrt(x * x + y * y + z * z);
+	}
+	real lenxy() const
+	{
+		return sqrt(x * x + y * y);
+	}
+	real sqrlenxy() const
+	{
+		return (x * x + y * y);
+	}
+	real lenxz() const
+	{
+		return sqrt(x * x + z * z);
+	}
+	real sqrlen() const
+	{
+		return (x * x + y * y + z * z);
+	}
+	bool norm()
+	{
+		real r = len();
+		if (r > 1e-6)
+		{
+			x /= r;
+			y /= r;
+			z /= r;
+			return true;
+		}
+		return false;
+	}
+	vector3 normcopy() const
+	{
+		real r = len();
+		if (r > 0)
+		{
+			return vector3(this->x / r,
+				this->y / r,
+				this->z / r);
+		}
+		return vector3::ZERO;
+	}
+	void fromanglelength(real _angle, real _r)
+	{
+		x = _r * cos(_angle);
+		y = _r * sin(_angle);
+	}
+	real angle() const
+	{
+		return atan2(y, x);
+	}
+	real dot(const vector3& v) const
+	{
+		return x * v.x + y * v.y + z * v.z;
+	}
+	vec3 crossdot(const vector3& n) const
+	{
+		const vec3& v = (*this);
+		return v - n * v.dot(n);
+	}
+	vector3 cross(const vector3& v) const
+	{
+		vector3 n;
+		n.x = -(y * v.z - z * v.y);
+		n.y = -(z * v.x - x * v.z);
+		n.z = -(x * v.y - y * v.x);
+		return n;
+	}
+	void rot(real angle, const vector3& ax);
+	vector3 rotcopy(real angle, const vector3& ax) const;
+	vector3 rotcpy(real angle, const vector3& ax) const;
+	static vector3 rnd(real min = 0, real max = 1);
+
+	static vector3 rndrad(real r = 1);
+};
+
+const vector3 vector3::ZERO = vector3(0, 0, 0);
+const vector3 vector3::ONE = vector3(1, 1, 1);
+const vector3 vector3::UX = vector3(1, 0, 0);
+const vector3 vector3::UY = vector3(0, 1, 0);
+const vector3 vector3::UZ = vector3(0, 0, 1);
+const vector3 vector3::CENTER = vector3(0.0, 0.0, MAXZ / 2);
+real vector3::sEPSINON = EPSINON;
+// ----------------------------------------
+vector3 vector3::rnd(real min, real max)
+{
+	return vector3(rrnd(min, max), rrnd(min, max), rrnd(min, max));
+}
+// ----------------------------------------
+vector3 vector3::rndrad(real r)
+{
+	return rnd(-1, 1).normcopy() * r;
+}
+// **********************************************************************
+// 4D
+// **********************************************************************
+struct vector4
+{
+	static const vector4 ZERO;
+	static const vector4 UX;
+	static const vector4 UY;
+	static const vector4 UZ;
+	static const vector4 UW;
+	static const vector4 CENTER;
+	union {
+		real val[4];
+		struct
+		{
+			float x;
+			float y;
+			float z;
+			float w;
+		};
+	};
+	vector4()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+		w = 0;
+	}
+	vector4(float _x, float _y, float _z, float _w)
+	{
+		x = _x;
+		y = _y;
+		z = _z;
+		w = _w;
+	}
+	vector4(float _x, crvec _v3)
+	{
+		x = _x;
+		y = _v3.x;
+		z = _v3.y;
+		w = _v3.z;
+	}
+	vector4(float _v)
+	{
+		x = _v;
+		y = _v;
+		z = _v;
+		w = _v;
+	}
+	real& operator [](int ind) {
+		return val[ind];
+	}
+	vector4 operator + (const vector4& _p) const
+	{
+		vector4 fp;
+		fp.x = x + _p.x;
+		fp.y = y + _p.y;
+		fp.z = z + _p.z;
+		fp.w = w + _p.w;
+		return fp;
+	}
+	void operator += (const vector4& _p)
+	{
+		x += _p.x;
+		y += _p.y;
+		z += _p.z;
+		w += _p.w;
+	}
+	vector4 operator - (const vector4& _p) const
+	{
+		vector4 fp;
+		fp.x = x - _p.x;
+		fp.y = y - _p.y;
+		fp.z = z - _p.z;
+		fp.w = w - _p.w;
+		return fp;
+	}
+	void operator -= (const vector4& _p)
+	{
+		x -= _p.x;
+		y -= _p.y;
+		z -= _p.z;
+		w -= _p.w;
+	}
+	vector4 operator - () const
+	{
+		vector4 fp;
+		fp.x = -x;
+		fp.y = -y;
+		fp.z = -z;
+		fp.w = -w;
+		return fp;
+	}
+	vector4 operator * (float s) const
+	{
+		vector4 fp;
+		fp.x = s * x;
+		fp.y = s * y;
+		fp.z = s * z;
+		fp.w = s * w;
+		return fp;
+	}
+	void operator *= (float s)
+	{
+		x = s * x;
+		y = s * y;
+		z = s * z;
+		w = s * w;
+	}
+	vector4 operator / (float s) const
+	{
+		vector4 fp;
+		fp.x = x / s;
+		fp.y = y / s;
+		fp.z = z / s;
+		fp.w = w / s;
+		return fp;
+	}
+	void operator /= (float s)
+	{
+		x = x / s;
+		y = y / s;
+		z = z / s;
+		w = w / s;
+	}
+	bool operator == (const vector4& rv) const
+	{
+		return (fabs(x - rv.x) <= EPSINON && fabs(y - rv.y) <= EPSINON && fabs(z - rv.z) <= EPSINON && fabs(w - rv.w) <= EPSINON);
+	}
+	bool operator != (const vector4& rv) const
+	{
+		return (fabs(x - rv.x) > EPSINON || fabs(y - rv.y) > EPSINON || fabs(z - rv.z) > EPSINON || fabs(w - rv.w) > EPSINON);
+	}
+	float len() const
+	{
+		return sqrt(x * x + y * y + z * z + w * w);
+	}
+	float sqrlen() const
+	{
+		return (x * x + y * y + z * z + w * w);
+	}
+	real norm()
+	{
+		float r = len();
+		if (r > 1e-5)
+		{
+			x /= r;
+			y /= r;
+			z /= r;
+			w /= r;
+		}
+		else
+		{
+			ERRORMSG("r == 0");
+		}
+		return r;
+	}
+	vector4 normcopy()
+	{
+		float r = len();
+		if (r > 0)
+		{
+			return vector4(
+				this->x / r,
+				this->y / r,
+				this->z / r,
+				this->w / r
+			);
+		}
+		else
+		{
+			ERRORMSG("r == 0");
+		}
+		return vector4(0, 0, 0, 0);
+	}
+	float dot(const vector4& v) const
+	{
+		return x * v.x + y * v.y + z * v.z + w * v.w;
+	}
+
+	// Cross4 computes the four-dimensional cross product of the three vectors
+	// U, V and W, in that order. It returns the resulting four-vector.
+
+	vector4 cross(const vector4& V, const vector4& W) const
+	{
+		vector4 result;
+		double A, B, C, D, E, F;       // Intermediate Values
+
+		// Calculate intermediate values.
+
+		A = (V.x * W.y) - (V.y * W.x);
+		B = (V.x * W.z) - (V.z * W.x);
+		C = (V.x * W.w) - (V.w * W.x);
+		D = (V.y * W.z) - (V.z * W.y);
+		E = (V.y * W.w) - (V.w * W.y);
+		F = (V.z * W.w) - (V.w * W.z);
+
+		// Calculate the result-vector components.
+
+		result.x = (y * F) - (z * E) + (w * D);
+		result.y = -(x * F) + (z * C) - (w * B);
+		result.z = (x * E) - (y * C) + (w * A);
+		result.w = -(x * D) + (y * B) - (z * A);
+
+		return result;
+	}
+};
+// **********************************************************************
+const vector4 vector4::ZERO = vector4(0, 0, 0, 0);
+const vector4 vector4::UX = vector4(1, 0, 0, 0);
+const vector4 vector4::UY = vector4(0, 1, 0, 0);
+const vector4 vector4::UZ = vector4(0, 0, 1, 0);
+const vector4 vector4::UW = vector4(0, 0, 0, 1);
+const vector4 vector4::CENTER = vector4(0.5, 0.5, 0.5, 0.5);
