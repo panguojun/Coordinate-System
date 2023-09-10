ad1ce5ed7a1c5de3c653d6b2d0d8c0a804879b14
panguojun <panguojun@users.noreply.github.com>
2023-09-10 05:41:32 +0000

    Update unit.hpp

--- unit.hpp
+++ unit.hpp
@@ -1,6 +1,6 @@
-/**    【插值单元对象】
-*      通过乘法可以计算插值
-*      使用插值单元可以代替线性函数
+/**         【插值单元对象】
+*         通过乘法可以计算插值
+*       使用插值单元可以代替线性函数
 */
 template<typename T>
 struct unit
@@ -16,4 +16,4 @@ struct unit
 template<typename T>
 const T unit<T>::ZERO = T(0);
 template<typename T>
-const T unit<T>::ONE = T(1);
+const T unit<T>::ONE  = T(1);
