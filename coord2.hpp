1a06a055d57b99cdd2549abc6b8587cd2b47b7fb
panguojun <panguojun@users.noreply.github.com>
2023-11-10 13:31:14 +0800

    Update coord2.hpp

--- coord2.hpp
+++ coord2.hpp
@@ -1,34 +1,39 @@
-/*********************************************************************
-*				【坐标系】
-*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
-* 	坐标系类是我单独封装，用于简化坐标变换，衍生出许多算法，能解决一些
-* 	坐标系变换相关的问题。
-* 	坐标系的运算跟李群很相似。
-*	坐标系由三个部分组成：C = M(位置） + S（缩放） * R（旋转）
+/************************************************************************************************
+*				[Coordinate System]
+*				   by Guojun Pan
+*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
+*   The coordinate system class is separately encapsulated by me for
+*   simplifying coordinate transformation and deriving many algorithms,
+*   which can solve some problems related to coordinate system transformation.
+*   The operation of the coordinate system is similar to Lie group.
+*   The coordinate system consists of three parts: C = M (position) + S (scaling) * R (rotation).
 *
-*  *  *  *  *  *  *  *  *  *  详解  *  *  *  *  *  *  *  *  *  *  *  *
-*	坐标系变换分为投影（/), 平移（^), 还原（*）三个步骤，
-*	坐标系本体符号 C，坐标系在单位距离上的变换梯度可以写成：
-*			G = C2 / C1 - I,
-*	除法分为两种（左，右）：
-*			oper(/)  = C1 * C2^-1
-*			oper(\)  = C1^-1 * C2
-*	在曲面上可以进行微分几何运算：
-*	定义一个内禀坐标系(假设它是平直空间，向量可以随意移动而不变)下V,在弯
-*	曲坐标系下观察V，不同点上V是不同的，故而坐标系跟位置有关，取相邻两点
-*	（1),(2)点处有向量V1,V2，对应坐标系C1,C2，那么：
-*			V = V1 * C1 = V2 * C2 =>
-*			V2 = V1 * C1 / C2, 令 G12 = C1 / C2 =>
-*			V2 = V1 * G12
+*  *  *  *  *  *  *  *  *  *  *  *  Detailed Explanation  *  *  *  *  *  *  *  *  *  *  *  *  *  *
+*   The coordinate system transformation is divided into three steps:
+*   			projection (/), translation (^), and restoration (*).
 *
-*	可以使用坐标系计算空间曲率，在u,v坐标系下黎曼曲率张量为：
-*			Ruv = Gu*Gv - Gv*Gu - G[u,v]
-*			其中：Gu = C2 / C1 - I
-*			     W = [u, v] : 李括号运算
-*			     G[u,v] = Gu*Wu + Gv*Wv
+*   The symbol of the coordinate system itself is C. The transformation between coordinate systems
+*   can be written as G = C2 / C1 - I, where G means gradient.
+*           		oper(/)  =  C1 * C2^-1
+*           		oper(\)  =  C1^-1 * C2
+*
+*   Specifically:
+*   Define an intrinsic coordinate system (assuming it is a flat space, and the vector can move freely
+*   without changing) under V. Observing V in a curved coordinate system, V is different at different points.
+*   Therefore, the coordinate system is related to the position.
+*   Take vectors V1 and V2 at adjacent points (1) and (2) respectively,
+*   corresponding to coordinate systems C1 and C2. Then:
+*           		V  = V1 * C1 = V2 * C2 =>
+*           		V2 = V1 * C1 / C2, let R12 = C1 / C2 =>
+*           		V2 = V1 * R12
+*
+*   The coordinate system can be used to calculate spatial curvature. In the u,v coordinate system,
+*   the Riemann curvature tensor is:
+*           	Ruv  = 	Gu*Gv - Gv*Gu - G[u,v]
+*           	where:  Gu = C2 / C1 - I
+*                   	Connection vector: W = [U, V] (Lie bracket operation)
+*                   	G[u,v] = Gu*Wu + Gv*Wv
 */
-
-//#define	Parallel_Projection		 // 非正交坐标系下平行投影
 // *******************************************************************
 //  |_
 // UC     2d Rotation Coordinate System
