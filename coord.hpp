cabcb3c61a99c4f002a21bb8e85454c8106b7e71
panguojun <panguojun@users.noreply.github.com>
2025-06-11 09:28:29 +0800

    Update coord.hpp

--- coord.hpp
+++ coord.hpp
@@ -23,12 +23,12 @@
 *   Therefore, the coordinate system is related to the position.
 *   Take vectors V1 and V2 at adjacent points (1) and (2) respectively,
 *   corresponding to coordinate systems C1 and C2. Then:
-*           		V  = V1 * C1 = V2 * C2 =>
-*           		V2 = V1 * C1 / C2, let R12 = C1 / C2 =>
+*           		V  = V1 / C1 = V2 / C2 =>
+*           		V2 = V1 * C2 / C1, let R12 = C2 / C1 =>
 *           		V2 = V1 * R12
 *
 *   The coordinate system can be used to calculate spatial curvature. In the u,v coordinate system,
-*   the Riemann curvature tensor is:
+*   the Curvature tensor is:
 *           	Ruv  = 	Gu*Gv - Gv*Gu - G[u,v]
 *           	where:  Gu = C2 / C1 - I
 *                   	Connection vector: W = [U, V] (Lie bracket operation)
